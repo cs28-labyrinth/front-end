ae4139a338e7c660dccc83187d3f27fc45906942
Eric Richards <esholundr@yahoo.com>
2020-04-28 10:45:43 -0600

    wrote login file

---
+++ labyrinth/src/components/Login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
+export default function Login() {
+	let history = useHistory();
+
+	return (
+		<Form>
+			<Formik
+				initialValues={{ username: '', password: '' }}
+				onSubmit={(values) => {
+					axiosWithAuth()
+						.post('https://------', { ...values })
+						.then((res) => {
+							localStorage.setItem('token', res.data.token);
+							history.push('/mainpage');
+						})
+						.catch((err) => console.log(err));
+				}}
+			>
+				<Form>
+					<span>Username: </span>
+					<Field type="username" name="username"></Field>
+
+					<span>Password: </span>
+					<Field type="password" name="password"></Field>
+
+					<button type="submit">Log In</button>
+				</Form>
+			</Formik>
+		</Form>
+	);
+}
