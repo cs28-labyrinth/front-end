70f47e258d66f9ed5e9a41fb9401dda04d6a0fc0
Eric Richards <esholundr@yahoo.com>
2020-04-30 16:06:13 -0600

    finished navbar, added MapPage to be completed later

--- labyrinth/src/App.js
+++ labyrinth/src/App.js
@@ -6,18 +6,21 @@ import Login from './components/Login';
 import Register from './components/Register';
 import MainPage from './components/MainPage';
 import PrivateRoute from './components/PrivateRoute';
-import Navigation from "./components/Navigation";
+import Navigation from './components/Navigation';
+import MapPage from './components/MapPage';
+
 function App() {
 	return (
 		<div className="App">
 			<h1>The Labyrinth</h1>
-			<Navigation></Navigation>
+			<Navigation />
 			<header className="App-header">
 				<Switch>
 					<Route exact path="/login" component={Login} />
 					<Route exact path="/register" component={Register} />
 
 					<PrivateRoute exact path="/mainpage" component={MainPage} />
+					<PrivateRoute exact path="/mappage" component={MapPage} />
 				</Switch>
 			</header>
 		</div>
