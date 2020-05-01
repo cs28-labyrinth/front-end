e0be284a1c558c04e2decfaba731cdae7957d4c3
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-05-01 02:10:22 -0500

    Updates

---
+++ precache-manifest.df6e5b6f62aebe9ab83a39a3d0a6fea0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c3547a6595ba40ac44f36c1c7e2bf6",
+    "url": "/front-end/index.html"
+  },
+  {
+    "revision": "f1b5969ff7fa5c2c7a73",
+    "url": "/front-end/static/css/main.b6ffdec3.chunk.css"
+  },
+  {
+    "revision": "0901308281811883330b",
+    "url": "/front-end/static/js/2.988d7923.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/front-end/static/js/2.988d7923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b5969ff7fa5c2c7a73",
+    "url": "/front-end/static/js/main.8e80d2a1.chunk.js"
+  },
+  {
+    "revision": "f7bd50155f5c81714433",
+    "url": "/front-end/static/js/runtime-main.124cd2c0.js"
+  }
+]);
